3d4c7047f5eb835470f114da5081280c675bc8b3
Ariole Dieujuste <arioledieujuste@gmail.com>
2022-07-26 07:14:10 -0400

    My first connection with express via Javascript

--- index.js
+++ index.js
@@ -1,49 +1,73 @@
 // import express library
-import express, { request, response } from "express";
+import express from 'express';
 
 // initialize express server
 const app = express();
 
-
-app.post('/students', (request, response) => {
-const newStudent = request.body;
-students.push(newStudent);
-response.status(201).send(students);
-
-
-})
-
-// List requests that we want express to respond to
-const students = [
-    { firstName: "Johnathan", lastName: "Vegas" },
-    { firstName: "Bridgette", lastName: "Lemus" },
-    { firstName: "Mason", lastName: "Madaras" },
-    { firstName: "Logan", lastName: "McCalley" },
-    { firstName: "Sebastien", lastName: "Placide" },
-
-];
-
-app.get("/hello", (request, response) => {
+// list requests that we want express to respond to
+app.get('/hello', (request, response) => {
   // do something...
-  console.log("Hello World🤪!");
-  response.send("Hello World!");
+  response.send('Hello World 🌎!');
 });
 
 
-app.get("/students", (request, response) => {
-   response.send(students)
-    
-});
-
-
-app.get("/students/:firstName", (request, response) => {
-const student = students.find(stud => stud.firstName === request.params.firstName)
-
-response.send(student);
-
-});
-
-// start listening on port
+// start listening on a port
 app.listen(3001, () => {
-  console.log("Now listening on port 3001");
+  console.log('Now listening on port 3001');
 });
+
+
+
+
+// *************** TESTING ******************
+
+// // import express library
+// import express, { request, response } from "express";
+
+// // initialize express server
+// const app = express();
+// app.use(express.json());
+
+
+// app.post('/students', (request, response) => {
+// const newStudent = request.body;
+// students.push(newStudent);
+// response.status(201).send(students);
+
+
+// })
+
+// // List requests that we want express to respond to
+// const students = [
+//     { firstName: "Johnathan", lastName: "Vegas" },
+//     { firstName: "Bridgette", lastName: "Lemus" },
+//     { firstName: "Mason", lastName: "Madaras" },
+//     { firstName: "Logan", lastName: "McCalley" },
+//     { firstName: "Sebastien", lastName: "Placide" },
+
+// ];
+
+// app.get("/hello", (request, response) => {
+//   // do something...
+//   console.log("Hello World🤪!");
+//   response.send("Hello World!");
+// });
+
+
+// app.get("/students", (request, response) => {
+//    response.send(students)
+    
+// });
+
+
+// app.get("/students/:firstName", (request, response) => {
+// const student = students.find(stud => stud.firstName === request.params.firstName)
+
+// response.send(student);
+
+// });
+
+// // start listening on port
+// app.listen(3001, () => {
+//   console.log("Now listening on port 3001");
+// });
